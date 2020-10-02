244e2c959f56d7cf703c7ee9e9fb2e84cd29477a
Blair MacIntyre <blair@macmynatt.com>
2020-10-02 10:59:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Assignment 1:  Different Architectures
+
+This is a three part assignment where you will implement a simple VR experience in three different programming environments: Babylon.js, Playcanvas, and A-Frame.
+
+The goal of the assignment is to have you gain experience with three very different architectures, and to reflect on these differences.  [Babylon.js](https://babylonjs.org) is an object-hierarchy architecture, similar to other systems like three.js and numerous non-web graphics libraries.  [Playcanvas](https://playcanvas.com) uses an Entity-Component architecture, similar to Unity, Unreal, and other game engines.  ~~[A-Frame](https://aframe.io) is a mix of the two, and is implement using custom DOM elements so that the scene graph sits in the HTML page.~~  For the third part, you will translate your Playcanvas Editor project to using only the underlying Playcanvas Engine.
+
+This repository is for the third of the three parts of the assignment, where you will use the Playcanvas Engine.
+
+# Assignment 1(c):  PlayCanvas VR Experience in Code
+
+This is a skeleton, based on converting the Playcanvas XR Starter project to the Playcanvas Engine. As such, you will be able to do a similar set of modifications to it as you did to the Editor sample for 1(b).  It uses a similar structure for local building and development as our previous Babylon projects, so you can run and test as you have for Babylon.
+
+For this assignment, you will convert your Assignment 1(b) solution to use the Engine.  The engine is available on [github](https://github.com/playcanvas/engine), and contains many examples.  We have built and included a copy of the engine here (including building source maps and Typescript .d.ts files). (note: we [fixed a few omissions and errors in the .d.ts files](https://github.com/playcanvas/engine/issues/2458), so you probably don't want to rebuild them from source until those changes are made in the engine repo.)  However, please download the engine so you can refer to the examples and source as needed.
+
+The learning objective of this assignment is to more fully understand how Playcanvas works and how implementing a project using just code can have a very similar structure to using Unity or Playcanvas.  You will also see how there are opportunities to structure your code that cross between the Playcanvas Editor (and Unity) "attach code as scripts to nodes in the graph" and "merge the code into the main line of the project."
+
+## Due: Friday October 9th, 11:59am (noon)
+
+## Name and GT user-id
+
+Name: 
+User ID:
+
+## Rubric
+
+Graded out of 5. You should be able to get much the same grade as you got on a(b) since you can reuse much of that code.
+
+0. Ensure your program builds with no Typescript warnings. (1)
+2. Add a 2D GUI panel attached to the left controller that has a color picker and two buttons ("cube" and "sphere"). (1)
+3. When you click, create a cube or sphere as described below, with the correct color. (2)
+4. Pressing the grip buttons on both controllers erases all the objects created. (1)
+
+There will be no bonus part for this part of the assignment.
+
+## Overview 
+
+The goal of the assignment is to use the Playcanvas Engine to create the same immersive WebXR application you just created in the Playcanvas Editor.
+
+This repository implements the same WebXR starter code project you used for 1(b), so you can take your solution to 1(b) and replicate the changes you made here.  
+
+You do not need to implement additional interaction functionality beyond 1(b).  Refer to that assignment for the requirements.
+
+One common complaint about the model used by Unity and Playcanvas is that all code must be attached to scripts.  The common result is that there are a number of scripts attached to the "Root" node, that contain global functionality.  In this sample project, that code is in the "vr.js", "object-picker.js", and "controllers.js" scripts.  
+
+Therefore, in addition to getting your assignment running, you must restructure your code to remove those scripts and put their functionality into "index.ts".  You can continue to use Playcanvas functionality (e.g., object-picker.js uses "this.app.on('object:pick', this.pick, this);", and that is ok, since it allows scripts to communicate with code in the main code file.)
+
+(Note: there are nice reasons to have this code separated into separate modules, even if it's not structured as a PlayCanvas "script", but we're moving it to our single code file for illustrative purposes.)
+
+# Collaboration
+
+You are free to discuss technical questions and issues in the Teams channels, but the code you submit should be your own.  So, please do not post large chunks of code, but providing pointers to examples or documentation pages or classes and methods, along with discussion of how to use them, is fine.
+
+# Submission
+
+You will check out the project from github classroom, and submit it there.  The project folder should contain just the additions to the sample project that are needed to implement the project.  Do not add extra files, and do not remove the .gitignore file (we do not want the "node_modules" directory in your repository.)
+
+**Do Not Change the names** of the existing files (e.g., index.html, index.ts, etc).  The TAs need to be able to test your program as follows:
+
+1. cd into the directory and run ```npm install```
+2. start a local web server and compile by running ```npm run start``` and pointing the browser at your ```index.html```
+
+Please test that your submission meets these requirements.  For example, after you check in your final version of the assignment to github, check it out again to a new directory and make sure everything builds and runs correctly.
+ 
+# Development Environment
+
+The sample has already been set up with a complete project for Typescript development, and includes a copy of the Playcanvas engine.
+
+## Running 
+
+You set up the initial project by pulling the dependencies from npm with 
+```
+npm install
+```
+
+After that, you can compile and run a server with:
+```
+npm run start
+```
+
+You do not have to run ```tsc``` to build the .js files from the .ts files;  ```npx``` builds them on the fly as part of running webpack.
+
+You can run the sample by pointing your web browser at ```https://localhost:8080/index.html```
+
+## License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Material for 3D User Interfaces Fall 2020</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.blairmacintyre.me/3dui-class-f20" property="cc:attributionName" rel="cc:attributionURL">Blair MacIntyre</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+The intent of choosing (CC BY-NC-SA 4.0) is to allow individuals and instructors at non-profit entities to use this content.  This includes not-for-profit schools (K-12 and post-secondary). For-profit entities (or people creating courses for those sites) may not use this content without permission (this includes, but is not limited to, for-profit schools and universities and commercial education sites such as Corsera, Udacity, LinkedIn Learning, and other similar sites).   
---
+++ src/index.ts
@@ -0,0 +1,515 @@
+/// <reference path="../playcanvas/playcanvas.d.ts" />
+(function () {
+    // we nest this inside a function (that we run immediately) so we can short circuit it 
+    // by doing a "return" if there is an error
+
+    // handy method to pop an error into the DOM
+    var displayError = function (html: string) {
+        var div = document.createElement('div');
+
+        div.innerHTML  = [
+            '<table style="background-color: #8CE; width: 100%; height: 100%;">',
+            '  <tr>',
+            '      <td align="center">',
+            '          <div style="display: table-cell; vertical-align: middle;">',
+            '              <div style="">' + html + '</div>',
+            '          </div>',
+            '      </td>',
+            '  </tr>',
+            '</table>'
+        ].join('\n');
+
+        document.body.appendChild(div);
+    };
+
+    ////////////////
+    // Initialize the engine.  Some of this was taken from one of the samples,
+    // and some from the Scene settings in the Playcanvas Editor.
+    var canvas = document.getElementById('application-canvas') as HTMLCanvasElement;
+    var app: pc.Application;
+    try {
+        app = new pc.Application(canvas, {
+            elementInput: new pc.ElementInput(canvas, {
+                useMouse: true,
+                useTouch: true
+            }),
+            mouse: new pc.Mouse(canvas),
+            touch: new pc.TouchDevice(canvas),
+            gamepads: new pc.GamePads(), 
+            keyboard: new pc.Keyboard(window),
+            graphicsDeviceOptions: {
+                'antialias': true,
+                'alpha': false,
+                'preserveDrawingBuffer': false,
+                'preferWebGl2': true
+            },
+            assetPrefix: '../assets/'
+        });
+    } catch (e) {
+        displayError('Could not initialize application. Error: ' + e);
+        return;
+    }
+
+    // for full screen and high resolution
+    app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
+    app.setCanvasResolution(pc.RESOLUTION_AUTO);
+    app.graphicsDevice.maxPixelRatio = window.devicePixelRatio;
+
+    // various settings to match the Editor project
+    app.scene.toneMapping = pc.TONEMAP_ACES;
+    app.scene.ambientLight = new pc.Color(51/255.0,51/255.0,51/255.0);
+    app.scene.exposure = 1.5;
+    app.scene.gammaCorrection = pc.GAMMA_SRGB;
+    app.scene.fog = pc.FOG_NONE;
+
+    // For baked lights
+    app.scene.lightmapSizeMultiplier = 32;
+    app.scene.lightmapMode = pc.BAKE_COLOR;
+    app.scene.lightmapMaxResolution = 2048;
+
+    window.addEventListener("resize", function () {
+        app.resizeCanvas(canvas.width, canvas.height);
+    });
+
+    ///////////////////
+    // ASSETS!
+    // There is an asset loader in the AssetRegistry (app.assets) but it doesn't appear to 
+    // handle models (with the hierarchy of materials and textures) well.  So, we'll just 
+    // use "app.assets.loadFromUrl" for each of the assets we have.
+
+    var assetList = [
+        {
+            type: 'model', 
+            url: 'models/box-room/box-room.json'
+        },
+        { 
+            type: 'material', 
+            url: 'materials/Box.json'
+        },
+        {
+            type: 'material', 
+            url: 'materials/Controller.json'
+        },
+        {
+            type: 'material', 
+            url: 'materials/Light.json'
+        },
+        {
+            type: 'script', 
+            url: 'scripts/vr.js'
+        },
+        {
+            type: 'script', 
+            url: 'scripts/controllers.js'
+        },
+        {
+            type: 'script', 
+            url: 'scripts/controller.js'
+        },   
+        {
+            type: 'script', 
+            url: 'scripts/object-picker.js'
+        },    
+        {
+            type: 'script', 
+            url: 'scripts/camera-controller.js'
+        },
+        {
+            type: 'texture', 
+            url: 'ui/cursor.png'
+        },
+        {
+            type: 'font', 
+            url: 'ui/Montserrat-Black.json'
+        }
+    ];
+
+    var count = 0;
+    assetList.forEach(function (a) {
+        app.assets.loadFromUrl(a.url,a.type, 
+            function(err, asset) {
+                count++;
+                if (count === assetList.length) {
+                    buildScene();
+                }
+            });
+    });
+
+    ///////////////////
+    // We will continue building the scene after the assets are loaded. We wrap the 
+    // rest of the creation process in a function so that it can get called only 
+    // after the assets have loaded
+    var buildScene = function () {
+
+        //////////////////// 
+        // NODES!
+
+        // FIRST, THE ROOM AND ALL STUFF IN IT
+
+        // the room entity and model
+        var roomEntity = new pc.Entity("Room");
+        app.root.addChild(roomEntity);
+        var model = app.assets.find('box-room.json', 'model');
+
+        // add model component to entity
+        roomEntity.addComponent("model", {
+            type: "asset",
+            asset: model,
+            castShadows: false,
+            castShadowsLightmap: true,
+            receiveShadows: true,
+            static:  false,
+            lightmapped: true,
+            lightmapSizeMultiplier: 1,
+            layers: [pc.LAYERID_WORLD]
+        });
+
+        var l = new pc.Entity("Down Dynamic");
+        l.translate(0,7.107,0);
+        l.addComponent("light", {
+            type: "directional",
+            color: pc.Color.WHITE,
+            intensity: 1,
+            isStatic: false,
+            bake: false,
+            affectDynamic: true,
+            castShadows: false,
+            layers: [pc.LAYERID_WORLD]
+        });
+        roomEntity.addChild(l);
+
+        l = new pc.Entity("Up Dynamic");
+        l.translate(0,7.107,0);
+        l.rotate(180,0,0);
+        l.addComponent("light", {
+            type: "directional",
+            color: new pc.Color(121/255.0,171/255.0,253/255.0),
+            intensity: 0.4,
+            affectDynamic: true,
+            castShadows: false,
+            layers: [pc.LAYERID_WORLD]
+        });
+        roomEntity.addChild(l);
+
+        // get the light sphere material from the assets
+        var lightMat = app.assets.find('Light.json', 'material');
+
+        // instead of creating 6 spheres one by one, we use a function to 
+        // create the same structure at different locations on the ceiling of
+        // the room.
+        var createLightSphere = function(x: number, y: number, z: number) {
+            var ls = new pc.Entity("Light Sphere");
+            ls.addComponent("model", {
+                type: "sphere",
+                materialAsset: lightMat,
+                layers: [pc.LAYERID_WORLD]
+            });
+            ls.setLocalScale(1, 1, 1);
+            ls.translate(x, y, z);
+
+            var l1 = new pc.Entity("Bounce");
+            l1.translate(0,-9.87,0);
+            l1.setEulerAngles(180,0,0);
+            l1.addComponent("light", {
+                type: "spot",
+                color: new pc.Color(121/255.0,171/255.0,253/255.0),
+                intensity: 1,
+                range: 20,
+                falloffMode: pc.LIGHTFALLOFF_INVERSESQUARED,
+                innerConeAngle: 20,
+                outerConeAngle: 80,
+                isStatic: false,
+                bake: true,
+                bakeDir: true,
+                affectDynamic: false,
+                affectLightMapped: false,
+                castShadows: false,
+                layers: [pc.LAYERID_WORLD]
+            });
+            ls.addChild(l1);
+            
+            l1 = new pc.Entity("Down");
+            l1.addComponent("light", {
+                type: "spot",
+                color: pc.Color.WHITE,
+                intensity: 4,
+                range: 30,
+                falloffMode: pc.LIGHTFALLOFF_INVERSESQUARED,
+                innerConeAngle: 40,
+                outerConeAngle: 75,
+                isStatic: false,
+                bake: true,
+                bakeDir: true,
+                affectDynamic: false,
+                affectLightMapped: false,
+                castShadows: true,
+                shadowResolution: 1024,
+                shadowDistance: 40,
+                shadowType: pc.SHADOW_VSM32,
+                vsmBlurMode: pc.BLUR_BOX,
+                vsmBlurSize: 5,
+                vsmBias: 0.05,
+                layers: [pc.LAYERID_WORLD]
+            });
+            ls.addChild(l1);        
+            roomEntity.addChild(ls);
+        }
+        createLightSphere(-9.472, 9.107, 10.767);
+        createLightSphere(-9.472, 9.107, -10.767);
+        createLightSphere(10.489, 9.107, -10.767);
+        createLightSphere(10.489, 9.107, 10.767);
+        createLightSphere(0.201, 9.107, -0.309);
+
+        // the floor is invisible, but it's bounding box
+        // is still used by the scripts.
+        var floor = new pc.Entity("Floor");
+        floor.addComponent("model", {
+            type: "box",
+            castShadows: true,
+            castShadowsLightmap: true,
+            receiveShadows: true,
+            isStatic: false,
+            lightmapped: false,
+            layers: [pc.LAYERID_WORLD]
+        });
+        floor.enabled = false;
+        floor.translate(0, -0.5, 0);
+        floor.setLocalScale(40,1,40);
+        floor.tags.add("pickable");
+        floor.tags.add("teleportable");  
+        roomEntity.addChild(floor);
+
+        // as with lights, we create 3 cubes using a functions
+        var cubeMat = app.assets.find('Box.json', 'material');
+        var createCube = function (x: number, y: number, z:number) {
+            var cube = new pc.Entity("Box");
+            cube.addComponent("model", {
+                type: "box",
+                materialAsset: cubeMat,
+                castShadows: false,
+                castShadowsLightmap: true,
+                receiveShadows: true,
+                isStatic: false,
+                lightmapped: true,
+                lightmapSizeMultiplier: 1,
+                layers: [pc.LAYERID_WORLD]
+            });
+            cube.setLocalScale(1, 1, 1);
+            cube.translate(x, y, z);
+            cube.tags.add("pickable");
+            cube.tags.add("interactive");
+            roomEntity.addChild(cube);
+        };
+        createCube(3,1,-5);
+        createCube(0,1,-5);
+        createCube(-3,1,-5);
+
+        ////////////////////
+        // NOW THE CAMERA
+
+        var cameraParent = new pc.Entity("Camera Parent");
+        app.root.addChild(cameraParent);
+        cameraParent.translate(0,1.572,4.249);
+
+        var camera = new pc.Entity("Camera");
+        camera.addComponent('camera', {
+            clearColor: new pc.Color(184 / 255, 184 / 255, 184 / 255)
+        });
+        cameraParent.addChild(camera);
+
+        cameraParent.addComponent("script");
+        cameraParent.script!.create("camera-controller", {
+            attributes: {
+                height: 1.6,
+                movementSpeed:1.5,
+                rotateSpeed:45,
+                rotateThreshold: 0.5,
+                rotateResetThreshold: 0.25,
+                camera: camera
+            }
+        });
+
+        //////////////////////
+        // NOW THE CONTROLLER template.  It will be instanciated twice.
+        // the Controller is disabled, and will be cloned.
+
+        var controller = new pc.Entity("Controller");
+        controller.enabled = false;
+        controller.translate(0,1.6,0);
+        controller.addComponent("script");
+        controller.script!.create("controller");
+        app.root.addChild(controller);
+
+        var controllerMat = app.assets.find('Controller.json', 'material');
+        var controllerModel = new pc.Entity("model");
+        controllerModel.addComponent("model", {
+            type: "box",
+            materialAsset: controllerMat,
+            castShadows: false,
+            castShadowsLightmap: false,
+            receiveShadows: true,
+            isStatic: false,
+            lightmapped: false,
+            layers: [pc.LAYERID_WORLD]
+        });
+        controllerModel.setLocalScale(0.05,0.05,0.05);
+        controller.addChild(controllerModel);
+
+        var pointerTexture = app.assets.find('cursor.png', 'texture');
+        var pointer = new pc.Entity("pointer");
+
+        pointer.addComponent("element", {
+            type: pc.ELEMENTTYPE_IMAGE,
+            anchor: new pc.Vec4(0.5,0.5,0.5,0.5),
+            pivot: new pc.Vec2(0.5,0.5),
+            width: 2,
+            height: 2,
+            color: pc.Color.WHITE,
+            opacity: 1,
+            rect: new pc.Vec4(0,0,1,1),
+            mask: false,
+            textureAsset: pointerTexture,
+            useInput: false,
+            layers: [pc.LAYERID_UI]
+        });
+        pointer.setLocalScale(0.01,0.01,0.01);
+        pointer.translate(0,0,-2);
+        controller.addChild(pointer);
+
+        ////////////////////////////
+        // Finally, the 2D SCREEN UI
+
+        var screen = new pc.Entity("2D Screen");
+        screen.translate(0,4,0);
+        screen.addComponent("screen", {
+            screenSpace: true,
+            referenceResolution: new pc.Vec2(1280,960),
+            scaleMode: pc.SCALEMODE_BLEND,
+            scaleBlend: 0.5
+        });
+        app.root.addChild(screen);
+
+        var button = new pc.Entity("Button");
+        var text = new pc.Entity("Text");
+        var textFont = app.assets.find('Montserrat-Black.json', 'font');
+        text.addComponent("element", {
+            type: pc.ELEMENTTYPE_TEXT,
+            anchor: new pc.Vec4(0.5,0.5,0.5,0.5),
+            pivot: new pc.Vec2(0.5,0.5),
+            autoWidth: true,
+            autoHeight: true,
+            alignment: new pc.Vec2(0.5,0.5),
+            fontAsset: textFont,
+            text: "VR",
+            fontSize: 48,
+            lightHeight: 48,
+            wrapLines: true,
+            spacing: 1,
+            color: pc.Color.WHITE,
+            opacity: 1,
+            outlineColor: pc.Color.BLACK,
+            outlineThickness: 0,
+            shadowColor: pc.Color.BLACK,
+            shadowOffset: new pc.Vec2(0,0),
+            useInput: false,
+            layers: [pc.LAYERID_UI]
+        });
+        button.addChild(text);
+
+        button.addComponent("button", {
+            transitionMode: pc.BUTTON_TRANSITION_MODE_TINT,
+            hoverTint: new pc.Color(1, 0.5,0),
+            pressedTint: pc.Color.WHITE,
+            inactiveTint: new pc.Color (0,0,20/255.0),
+            fadeDuration: 0
+        });
+        button.addComponent("element", {
+            type: pc.ELEMENTTYPE_IMAGE,
+            anchor: new pc.Vec4(0.5,0.5,0.5,0.5),
+            pivot: new pc.Vec2(0.5,0.5),
+            width: 196,
+            height: 196,
+            color: pc.Color.BLACK,
+            opacity: 0.4,
+            rect: new pc.Vec4(0,0,1,1),
+            mask: false,
+            useInput: true,
+            layers: [pc.LAYERID_UI]
+        });
+        button.button!.imageEntity = button;
+        screen.addChild(button);
+
+        var textMsg = function(name: string, text: string) {
+            var t = new pc.Entity(name);
+            t.translate(16,0,0);
+            t.addComponent("element", {
+                type: pc.ELEMENTTYPE_TEXT,
+                anchor: new pc.Vec4(0.5,0.5,0.5,0.5),
+                pivot: new pc.Vec2(0.5,0.5),
+                autoWidth: true,
+                autoHeight: true,
+                alignment: new pc.Vec2(0.5,0.5),
+                fontAsset: textFont,
+                text: text,
+                fontSize: 32,
+                lightHeight: 32,
+                wrapLines: true,
+                spacing: 1,
+                color: pc.Color.WHITE,
+                opacity: 1,
+                outlineColor: pc.Color.BLACK,
+                outlineThickness: 0,
+                shadowColor: pc.Color.BLACK,
+                shadowOffset: new pc.Vec2(0,0),
+                useInput: false,
+                layers: [pc.LAYERID_UI]
+            });
+            screen.addChild(t);
+            return t;
+        };
+        var textUnsupported = textMsg("Text Unsupported","WebXR is not supported");
+        var textHTTPSRequired = textMsg("Text HTTPS required","WebXR requires a secure connection (HTTPS)");
+        textHTTPSRequired.element!.autoWidth = false;
+        textHTTPSRequired.element!.width = 600;
+
+        /////////////////////
+        /// SCRIPTS!
+        /// doing this at the end since we need to reference a bunch of objects
+        /// that we created along the way.
+        //
+        // We are just leaving the scripts as-is. But, we could move some of them to be
+        // here as methods in the main code file if we wanted, especially 
+        // the "vr", "object-pickers", and "controllers" scripts which live on the root.
+        // 
+        app.root.addComponent("script");
+        app.root.script!.create("vr", {
+            attributes: {
+                buttonVr: button,
+                elementUnsupported: textUnsupported,
+                elementHttpsRequired: textHTTPSRequired,
+                cameraEntity: camera
+            }
+        });
+        app.root.script!.create("controllers", {
+            attributes: {
+                controllerTemplate: controller,
+                cameraParent: cameraParent
+            }
+        });
+        app.root.script!.create("objectPicker");
+
+        // back the lights before starting
+        app.lightmapper.bake(null, pc.BAKE_COLOR);
+
+        /// READY TO GO!
+        app.start();
+
+        //// not used here, but many of the engine examples add an update event on 
+        //// "app", which is essentially equivalent to the requestAnimationFrame update
+        //// loop that would be done in three.js and babylon.js for your main loop
+        //
+        // app.on("update", function (dt) {
+        //    // code for your main loop
+        // });
+    }
+})();
