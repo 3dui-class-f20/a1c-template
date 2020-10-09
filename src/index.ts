bff56178d94d6569ce5963ba01412fa59ca15af8
Blair MacIntyre <blair@macmynatt.com>
2020-10-09 09:11:13 -0400

    indentation

--- src/index.ts
+++ src/index.ts
@@ -532,117 +532,114 @@ function play() {
         // });
     }
 };
-    pc.script.createLoadingScreen(function (app) {
-        var showSplash = function () {
-            // splash wrapper
-            var wrapper = document.createElement('div');
-            wrapper.id = 'application-splash-wrapper';
-            document.body.appendChild(wrapper);
+pc.script.createLoadingScreen(function (app) {
+    var showSplash = function () {
+        // splash wrapper
+        var wrapper = document.createElement('div');
+        wrapper.id = 'application-splash-wrapper';
+        document.body.appendChild(wrapper);
 
-            // splash
-            var splash = document.createElement('div');
-            splash.id = 'application-splash';
-            wrapper.appendChild(splash);
-            splash.style.display = 'none';
+        // splash
+        var splash = document.createElement('div');
+        splash.id = 'application-splash';
+        wrapper.appendChild(splash);
+        splash.style.display = 'none';
 
-            var logo = document.createElement('img');
-            logo.src = 'a1c-splash.png';
-            splash.appendChild(logo);
-            logo.onload = function () {
-                splash.style.display = 'block';
-            };
-
-            var container = document.createElement('div');
-            container.id = 'progress-bar-container';
-            splash.appendChild(container);
-
-            var bar = document.createElement('div');
-            bar.id = 'progress-bar';
-            container.appendChild(bar);
-
+        var logo = document.createElement('img');
+        logo.src = 'a1c-splash.png';
+        splash.appendChild(logo);
+        logo.onload = function () {
+            splash.style.display = 'block';
         };
 
-        var createCss = function () {
-            var css = [
-                'body {',
-                '    background-color: #283538;',
-                '}',
+        var container = document.createElement('div');
+        container.id = 'progress-bar-container';
+        splash.appendChild(container);
 
-                '#application-splash-wrapper {',
-                '    position: absolute;',
-                '    top: 0;',
-                '    left: 0;',
-                '    height: 100%;',
-                '    width: 100%;',
-                '    background-color: #283538;',
-                '}',
+        var bar = document.createElement('div');
+        bar.id = 'progress-bar';
+        container.appendChild(bar);
 
-                '#application-splash {',
-                '    position: absolute;',
-                '    top: calc(50% - 28px);',
-                '    width: 264px;',
-                '    left: calc(50% - 132px);',
-                '}',
+    };
 
-                '#application-splash img {',
-                '    width: 100%;',
-                '}',
+    var createCss = function () {
+        var css = [
+            'body {',
+            '    background-color: #283538;',
+            '}',
 
-                '#progress-bar-container {',
-                '    margin: 20px auto 0 auto;',
-                '    height: 2px;',
-                '    width: 100%;',
-                '    background-color: #1d292c;',
-                '}',
+            '#application-splash-wrapper {',
+            '    position: absolute;',
+            '    top: 0;',
+            '    left: 0;',
+            '    height: 100%;',
+            '    width: 100%;',
+            '    background-color: #283538;',
+            '}',
 
-                '#progress-bar {',
-                '    width: 0%;',
-                '    height: 100%;',
-                '    background-color: #f60;',
-                '}',
-                '@media (max-width: 480px) {',
-                '    #application-splash {',
-                '        width: 170px;',
-                '        left: calc(50% - 85px);',
-                '    }',
-                '}'
+            '#application-splash {',
+            '    position: absolute;',
+            '    top: calc(50% - 28px);',
+            '    width: 264px;',
+            '    left: calc(50% - 132px);',
+            '}',
 
-            ].join('\n');
+            '#application-splash img {',
+            '    width: 100%;',
+            '}',
 
-            var style = document.createElement('style');
-            style.type = 'text/css';
-            style.appendChild(document.createTextNode(css));
-        
-            document.head.appendChild(style);
-        };
+            '#progress-bar-container {',
+            '    margin: 20px auto 0 auto;',
+            '    height: 2px;',
+            '    width: 100%;',
+            '    background-color: #1d292c;',
+            '}',
 
+            '#progress-bar {',
+            '    width: 0%;',
+            '    height: 100%;',
+            '    background-color: #f60;',
+            '}',
+            '@media (max-width: 480px) {',
+            '    #application-splash {',
+            '        width: 170px;',
+            '        left: calc(50% - 85px);',
+            '    }',
+            '}'
 
-        createCss();
+        ].join('\n');
 
-        showSplash();
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(css));
+    
+        document.head.appendChild(style);
+    };
 
+    createCss();
+
+    showSplash();
+});
+
+function setupSplash(app: pc.Application) {
+    var hideSplash = function () {
+        var splash = document.getElementById('application-splash-wrapper');
+        if (splash) {
+            splash!.parentElement!.removeChild(splash);
+        }
+    };
+
+    var setProgress = function (value: number) {
+        var bar = document.getElementById('progress-bar');
+        if (bar) {
+            value = Math.min(1, Math.max(0, value));
+            bar.style.width = value * 100 + '%';
+        }
+    };
+
+    app.on('preload:end', function () {
+        app.off('preload:progress');
     });
-
-    function setupSplash(app: pc.Application) {
-        var hideSplash = function () {
-            var splash = document.getElementById('application-splash-wrapper');
-            if (splash) {
-                splash!.parentElement!.removeChild(splash);
-            }
-        };
-    
-        var setProgress = function (value: number) {
-            var bar = document.getElementById('progress-bar');
-            if (bar) {
-                value = Math.min(1, Math.max(0, value));
-                bar.style.width = value * 100 + '%';
-            }
-        };
-    
-        app.on('preload:end', function () {
-            app.off('preload:progress');
-        });
-        app.on('preload:progress', setProgress);
-        app.on('start', hideSplash);
-    }
-    
+    app.on('preload:progress', setProgress);
+    app.on('start', hideSplash);
+}
